b63f42e8cd7c7d5969c0e79ff7efe03632542598
--add <rahmanaky521@gmail.com>
2023-10-23 22:16:44 +0600

    first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import BannerSection from "@/components/homePage/BannerSection";
 import Trending from "@/components/homePage/Trending";
 import GetMoviesData from "@/lib/GetMoviesData";
-import React, { Suspense } from "react";
 
 const HomePage = async ({ popularMovies }) => {
   const { results: trendingShows } = await GetMoviesData("trending/movie/day");
@@ -9,11 +8,8 @@ const HomePage = async ({ popularMovies }) => {
   return (
     <div className="md:-mt-[64px] space-y-14">
       <BannerSection popularMovies={popularMovies} />
-      <Suspense
-        fallback={<span className="loading loading-spinner loading-lg"></span>}
-      >
-        <Trending trendingShows={trendingShows} />
-      </Suspense>
+
+      <Trending trendingShows={trendingShows} />
     </div>
   );
 };
