ca6e34ac62d5569083a1dc7f7b82e035ceec121d
--add <rahmanaky521@gmail.com>
2023-10-24 12:55:32 +0600

    page error fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
+import BannerSection from "@/components/homePage/BannerSection";
+import Trending from "@/components/homePage/Trending";
+import LoadingBig from "@/components/shared/LoadingBig";
 import GetMoviesData from "@/lib/GetMoviesData";
-import HomePage from "@/pages/HomePage";
+import { Suspense } from "react";
 
 export default async function Home() {
   const { results: popularMovies } = await GetMoviesData("movie/popular");
+  const { results: trendingShows } = await GetMoviesData("trending/movie/day");
 
   return (
-    <main>
-      <HomePage popularMovies={popularMovies} />
+    <main className="space-y-10">
+      <BannerSection popularMovies={popularMovies} />
+      <Suspense fallback={<LoadingBig />}>
+        <Trending trendingShows={trendingShows} />
+      </Suspense>
     </main>
   );
 }
